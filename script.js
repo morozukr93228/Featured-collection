3cae3d2aafde6a37aa3e1c74594b824ff10b9e3c
morozukr93228 <morozukr93@gmail.com>
2026-03-04 09:18:49 -0800

    update drag working on desktop

--- script.js
+++ script.js
@@ -126,3 +126,30 @@ window.addEventListener('resize', () => {
   }
 
 });
+
+const slider = document.getElementById("productContainer");
+let isDown = false;
+let startX = 0;
+let scrollLeft = 0;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("cursor-grabbing");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+
+window.addEventListener("mouseup", () => {
+  isDown = false;
+  slider.classList.remove("cursor-grabbing");
+});
+
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 1;
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+slider.addEventListener("dragstart", (e) => e.preventDefault());
